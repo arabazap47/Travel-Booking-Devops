32883be798e3b07faf36f5403119380f948a5c39
arabazap47 <arabazpinjar@gmail.com>
2026-01-01 13:33:50 +0530

    Finalaize

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
   return (
     <footer className="bg-white border-t py-6 mt-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-sm text-gray-600">
-        © {new Date().getFullYear()} Travelly (India) Private Limited. All rights reserved
+        © {new Date().getFullYear()} Travelly (India) Private Limited. All rights reserved..
       </div>
     </footer>
   )
